70c1501d4d1e8478a143f1d12ce9d9fca04555a4
Marcel Joschko <marceljoschko@proton.me>
2024-01-05 12:21:46 +0100

    test

--- src/App.tsx
+++ src/App.tsx
@@ -26,7 +26,7 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">
-        TestTest on the Vite and React logos to learn more
+        TestTesttest on the Vite and React logos to learn more
       </p>
     </>
   )
