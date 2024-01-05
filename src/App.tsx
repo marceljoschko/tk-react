ff784449250c132b91236086008b605387d8b09f
Marcel Joschko <marceljoschko@proton.me>
2024-01-05 12:17:54 +0100

    test change

--- src/App.tsx
+++ src/App.tsx
@@ -26,7 +26,7 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">
-        Bla on the Vite and React logos to learn more
+        TestTest on the Vite and React logos to learn more
       </p>
     </>
   )
